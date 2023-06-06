91128ba4a2baa343eaa178a6eaad5dd384352e6a
BlackHold2K <cuteza456@gmail.com>
2023-06-06 15:07:51 +0700

    add health bar script

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,16 +6,14 @@ public class PlayerScript : MonoBehaviour
 {
     public float speed = 10f;
 
-    Rigidbody2D rigidbody2D;
+    public float maxHealth = 10f;
+    public float currentHealth = 0f;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
