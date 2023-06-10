739a29183a98d22e62e794c640fd9126818525ae
BlackHold2K <cuteza456@gmail.com>
2023-06-10 15:45:32 +0700

    add spawn item function

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -10,37 +10,44 @@ public class EnemySpawner : MonoBehaviour
 
     public GameObject enemy;
 
+    public List<GameObject> items = new List<GameObject>();    
+
     public List<GameObject> enemyList = new List<GameObject>();
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.E))
             SpawnEnemy();
+
+        if(Input.GetKeyDown(KeyCode.R))
+            SpawnItem();
     }
 
     void SpawnEnemy()
     {
-        // Camera mainCam = Camera.main;
-
-        // float cameraHight = 2f * mainCam.orthographicSize;
-        // float cameraWidth = cameraHight * mainCam.aspect;
-        // Vector3 randomPosition = new Vector3(Random.Range(-cameraWidth/2,cameraWidth/2),Random.Range(-cameraHight/2,cameraHight/2),0);
-
-        // Vector3 offScreenPosition = mainCam.transform.position + (randomPosition.normalized * spawnOffset);
-
-        // print($"x : {offScreenPosition}, y : {offScreenPosition}");
-        // Instantiate(enemy,offScreenPosition,Quaternion.identity);
-
         Vector3 playerPosition = player.position;
 
         Vector3 randomDiraction = new Vector3(Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f),0); //random diraction that enemy will spawn
 
         Vector3 offScreenPosition = playerPosition + (randomDiraction.normalized * spawnOffset); // position that enemy will spawn
 
-        print($"x : {offScreenPosition}, y : {offScreenPosition}");
         GameObject _enemy = Instantiate(enemy,offScreenPosition,Quaternion.identity); //spawn enemy
         enemyList.Add(_enemy);
     }
+
+    void SpawnItem()
+    {
+        Vector3 playerPosition = player.position;
+
+        Vector3 randomDiraction = new Vector3(Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f),0); //random diraction that enemy will spawn
+
+        Vector3 offScreenPosition = playerPosition + (randomDiraction.normalized * spawnOffset); // position that enemy will spawn
+
+        GameObject randomItem = items[Random.Range(0,items.Count)];
+
+        GameObject _item = Instantiate(randomItem,offScreenPosition,Quaternion.identity); //spawn enemy
+    }
+    
 
     public void DestroyAllEnemy()
     {
