e097be18f33d9ceb1e7a036a7f57b0385960cba4
BlackHold2K <cuteza456@gmail.com>
2023-06-13 20:29:51 +0700

    fix error enemyspawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,47 +8,52 @@ public class EnemySpawner : MonoBehaviour
 
     public Transform player;
 
-    public GameObject enemy;
-
-    public List<GameObject> items = new List<GameObject>();    
-
-    public List<GameObject> enemyList = new List<GameObject>();
     public float gameTime;
     public List<EnemySet> enemySet = new List<EnemySet>();
     int enemySetCount;
 
+    public float enemySpawnTime;
+    public List<GameObject> enemyList = new List<GameObject>();
+    List<GameObject> enemyContainer = new List<GameObject>();
+    float enemySpawnAmount; //when start new wave add enemySpawnAmount
+
+    Coroutine spawnSequence;
+
     void Start()
     {
+        enemySpawnAmount = 10;
+
         InvokeRepeating("SpawnSequence",1f ,1f);
+        // spawnSequence = StartCoroutine(SpawnSequence());
     }
 
-    private void Update() 
-    {
-        if(Input.GetKeyDown(KeyCode.E))
-            SpawnEnemy();
+    // IEnumerator SpawnSequence()
+    // {
+    //     GameObject _enemy = enemyList[Random.Range(0,enemyList.Count)];
 
-        if(Input.GetKeyDown(KeyCode.R))
-            SpawnItem();
-    }
+    //     for(int i = 0; i< enemySpawnAmount; i++)
+    //     {
+    //         SpawnEnemy(_enemy);
+    //     }
+        
+    //     yield return new WaitForSeconds(enemySpawnTime);
+
+    //     enemySpawnAmount += 5;
+    //     spawnSequence = StartCoroutine(SpawnSequence());
+    // }
 
     void SpawnSequence()
     {
         CountTime();
-        CheckSpawnTime();
+        // CheckSpawnTime();
+        CheckSpawnValue();
     }
 
     void SpawnEnemy(GameObject enemy)
     {
         GameObject _enemy = Instantiate(enemy,RandomSpawnPosition(),Quaternion.identity); //spawn enemy with random position function
         
-        enemyList.Add(_enemy);
-    }
-
-    void SpawnItem()
-    {
-        GameObject randomItem = items[Random.Range(0,items.Count)];
-
-        GameObject _item = Instantiate(randomItem,RandomSpawnPosition(),Quaternion.identity); //spawn enemy with random position function
+        enemyContainer.Add(_enemy);
     }
 
     Vector3 RandomSpawnPosition()
@@ -62,59 +67,59 @@ public class EnemySpawner : MonoBehaviour
         return offScreenPosition;
     }
     
-
     void CountTime()
     {
         gameTime += 1;
     }
 
-    void CheckSpawnTime()
-    {
-        CheckSetCountIncrease();
-        if(enemySet.Count - 1 == enemySetCount)
-        {
-            if(gameTime >= enemySet[enemySetCount].timeWave)
-            {
-                CheckSpawnValue();
-            }
-        }
-        else
-        {
-            if(gameTime >= enemySet[enemySetCount].timeWave 
-            && gameTime < enemySet[enemySetCount + 1].timeWave)
-            {
-                CheckSpawnValue();
-            }
-        }
-    }
+    // void CheckSpawnTime()
+    // {
+    //     CheckSetCountIncrease();
+    //     if(enemySet.Count - 1 == enemySetCount)
+    //     {
+    //         if(gameTime >= enemySet[enemySetCount].timeWave)
+    //         {
+    //             CheckSpawnValue();
+    //         }
+    //     }
+    //     else
+    //     {
+    //         if(gameTime >= enemySet[enemySetCount].timeWave 
+    //         && gameTime < enemySet[enemySetCount + 1].timeWave)
+    //         {
+    //             CheckSpawnValue();
+    //         }
+    //     }
+    // }
+    
     void CheckSpawnValue()
     {
-        int setCountTemp = 0;
-
         foreach(EnemySet enemySetTemp in enemySet)
         {
-            if(gameTime % enemySet[enemySetCount].enemySetDetail[setCountTemp].enemySpawnTime == 0)
+            foreach(EnemySetDetail enemySetDetail in enemySetTemp.enemySetDetail)
             {
-                for (int i = 0; i < enemySet[enemySetCount].enemySetDetail[setCountTemp].enemySpawnCount ; i++)
+                if(gameTime % enemySetDetail.enemySpawnTime == 0)
                 {
-                    SpawnEnemy(enemySet[enemySetCount].enemySetDetail[setCountTemp].enemyPrefab);
+                    for (int i = 0; i < enemySetDetail.enemySpawnCount ; i++)
+                    {
+                        SpawnEnemy(enemySetDetail.enemyPrefab);
+                    }
                 }
             }
-            setCountTemp++;
         }
     }
-    void CheckSetCountIncrease()
-    {
-        if(enemySet.Count - 1 == enemySetCount) { return; }
-        if(gameTime == enemySet[enemySetCount + 1].timeWave)
-        {
-            enemySetCount++;
-        }
-    }
+    // void CheckSetCountIncrease()
+    // {
+    //     if(enemySet.Count - 1 == enemySetCount) { return; }
+    //     if(gameTime == enemySet[enemySetCount + 1].timeWave)
+    //     {
+    //         enemySetCount++;
+    //     }
+    // }
 
     public void DestroyAllEnemy()
     {
-        foreach(GameObject _enemy in enemyList)
+        foreach(GameObject _enemy in enemyContainer)
         {
             Destroy(_enemy);
         }
