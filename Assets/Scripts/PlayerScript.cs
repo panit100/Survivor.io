2dfa0bf38e13b19130497366d53c2b42418564ca
BlackHold2K <cuteza456@gmail.com>
2023-06-20 16:22:59 +0700

    add animation and scound

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,10 +13,13 @@ public class PlayerScript : MonoBehaviour
     public int exp = 0;
     public int CurrentLevel = 1;
 
-    
+    public Animator animator;
+    AudioSource audio;
 
     void Start()
     {
+        audio = GetComponent<AudioSource>();
+
         Time.timeScale = 1;
         currentHealth = maxHealth;
     }
@@ -32,7 +35,24 @@ public class PlayerScript : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        if(horizontal == 0 && vertical == 0)
+        {
+            animator.Play("Idle");
+            audio.Stop();
+            return;
+        }
+
+        if(!audio.isPlaying)
+            audio.Play();
+        
         var diraction = new Vector2(horizontal,vertical).normalized;
+
+        if(diraction.x > 0)
+            animator.Play("WalkRight");
+        else if(diraction.x < 0)
+            animator.Play("WalkLeft");
+        else
+            animator.Play("WalkDown");
 
         transform.Translate(diraction * speed * Time.deltaTime);
 
