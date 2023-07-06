e3b8ec6983f029ef5ff733468518c17aa0cab055
BlackHold2K <cuteza456@gmail.com>
2023-07-06 14:47:15 +0700

    upgrade card

--- Assets/Scripts/UI/UpgradePanel.cs
+++ Assets/Scripts/UI/UpgradePanel.cs
@@ -1,19 +1,51 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UpgradePanel : MonoBehaviour
 {
+    [SerializeField] List<UpgradeCard> cards = new List<UpgradeCard>();
+    [SerializeField] PlayerWeapon playerWeapon;
+
+    List<BaseAttack> tempRandomAttacks;
+
+    public void SetCard()
+    {
+        gameObject.SetActive(true);
+
+        RandomUpgradeWeapon();
+        
+        for(int i = 0; i < cards.Count; i++)
+        {
+            cards[i].SetCardInfo(tempRandomAttacks[i]);
+        }
+    }
+
+    void RandomUpgradeWeapon()
+    {
+        tempRandomAttacks = new List<BaseAttack>();
+
+        for(int i = 0; i < cards.Count; i++)
+        {
+            BaseAttack randomAttack = playerWeapon.attacks[Random.Range(0,playerWeapon.attacks.Count)];
+            
+            while(tempRandomAttacks.Count < cards.Count)
+            {
+                if(!tempRandomAttacks.Contains(randomAttack))
+                    tempRandomAttacks.Add(randomAttack);
+                else
+                    randomAttack = playerWeapon.attacks[Random.Range(0,playerWeapon.attacks.Count)];
+            }
+        }
+    }
+
     private void OnEnable()
     {
         Time.timeScale = 0;
-        print(1);
     }
 
     private void OnDisable()
     {
         Time.timeScale = 1;
-        print(2);
     }
 }
