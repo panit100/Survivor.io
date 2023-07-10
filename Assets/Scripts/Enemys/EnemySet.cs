a5608028198d34b142705e8c966b2692cbd9ca88
BlackHold2K <cuteza456@gmail.com>
2023-07-10 19:47:25 +0700

    create enemy set script and fix exp lerp

---
+++ Assets/Scripts/Enemys/EnemySet.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemySet
+{
+    public float timeWave;
+    public List<EnemySetDetail> enemySetDetail;
+}
