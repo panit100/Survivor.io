1ce32315d7c8ae7fa675bb915a223638329a2c5b
Panit <cuteza456@gmail.com>
2023-07-26 15:08:12 +0700

    remove code from item script

--- Assets/lecture/8.Item/HealItemScript.cs
+++ Assets/lecture/8.Item/HealItemScript.cs
@@ -6,17 +6,9 @@ namespace TA
 {
     public class HealItemScript : MonoBehaviour
     {
-        int healAmount = 10;
-
         private void OnTriggerEnter2D(Collider2D other) 
         {
-            if(other.CompareTag("Player"))
-            {
-                PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
-                
-                playerHealth.Heal(healAmount);
-                Destroy(this.gameObject);
-            }
+
         }
     }
 }
